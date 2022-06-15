d72eef224710de154796250b65f963d5d728fa3d
Axel Brinck <axel.louis.1991@gmail.com>
2022-06-15 22:44:47 +0200

    is odd? example

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -2,11 +2,12 @@ import { render } from 'solid-js/web';
 import { createSignal } from 'solid-js';
 
 const App = () => {
-    const [counter, setCounter] = createSignal(0);
+    const [getCounter, setCounter] = createSignal(0);
     return (
         <>
             <h1>Counter</h1>
-            <p>{counter}</p>
+            <p>{getCounter()}</p>
+            <p>{getCounter() % 2 === 0 ? 'par' : 'impar'}</p>
             <button onClick={() => {
                 setCounter(counter => counter + 1);
             }}>Increment</button>
