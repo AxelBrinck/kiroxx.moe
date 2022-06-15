8bb1a4ef2e509ebccf8213d5703088576e42dd7e
Axel Brinck <axel.louis.1991@gmail.com>
2022-06-15 22:33:49 +0200

    test hello on body

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,7 +1,5 @@
-/* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
-import App from './App';
+const app = () => <h1>Hello</h1>
 
-render(() => <App />, document.getElementById('root'));
+render(app, document.body)
